2bcd6b07e103c7e6753efde12db47fe0654283df
bahareeskandari <bahareeskandarijou@gmail.com>
2020-07-20 21:17:04 +0200

    final ??

--- src/Util/Constants.js
+++ src/Util/Constants.js
@@ -5,8 +5,8 @@ if (process.env.NODE_ENV === 'production') {
   Keys = require('../Keys.json')
 }
 
-export const getTopMoviesUrl = (page) => `https://netflixtrailers.herokuapp.com/api/Movies?api_key=${Keys.REACT_APP_MOVIEKEY}&language=en-US&page=${page}`
-export const getTopRatedTvShowsUrl = (page) => `https://netflixtrailers.herokuapp.com/api/TVShows?api_key=${Keys.REACT_APP_MOVIEKEY}&language=en-US&page=${page}`
+export const getTopMoviesUrl = (page) => `https://netflixserver.herokuapp.com/api/movies?api_key=${Keys.REACT_APP_MOVIEKEY}&language=en-US&page=${page}`
+export const getTopRatedTvShowsUrl = (page) => `https://netflixserver.herokuapp.com/api/TVSHows?api_key=${Keys.REACT_APP_MOVIEKEY}&language=en-US&page=${page}`
 export const imageFirstPart = 'https://image.tmdb.org/t/p/w200/'
 
 // export const getTopMoviesUrl = (page) => `http://localhost:9000/movies?api_key=${Keys.REACT_APP_MOVIEKEY}&language=en-US&page=${page}`
