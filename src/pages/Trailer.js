75d4d9a110ffa85697423815212480607eab33cf
bahareeskandari <bahareeskandarijou@gmail.com>
2020-07-20 21:56:29 +0200

    ok everyhting working

--- src/pages/Trailer.js
+++ src/pages/Trailer.js
@@ -118,7 +118,7 @@ const Trailer = ({ movieId }) => {
   }
 
   const { user, setUser } = useContext(UserContext)
-  const urlYoutube = `https://netflixtrailers.herokuapp.com/api/trailer?part=snippet&key=${Keys.REACT_APP_APIKEYYOUTUBE}&q=${titleOf}%20trailer`
+  const urlYoutube = `https://netflixserver.herokuapp.com/api/trailer?part=snippet&key=${Keys.REACT_APP_APIKEYYOUTUBE}&q=${titleOf}%20trailer`
   //   const urlYoutube = `http://localhost:9000/api/trailer?part=snippet&key=${Keys.REACT_APP_APIKEYYOUTUBE}&q=${titleOf}%20trailer`
 
   useEffect(() => {
